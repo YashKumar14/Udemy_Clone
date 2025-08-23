9d6852dc92b54ffb064dd15efabf8273ebc0e2c2
YashKumar <marupillayeswanth123@gmail.com>
2025-08-23 23:36:10 +0530

    added global css file

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -3,6 +3,7 @@ import Antd from "ant-design-vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "../public/global.css";
 
 const app = createApp(App);
 
