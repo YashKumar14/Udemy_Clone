63e71d880b1f6a4270bf0b560b2861f618ee993f
YashKumar <marupillayeswanth123@gmail.com>
2025-08-23 23:41:09 +0530

    routes optimised

--- Backend/routes/routers.js
+++ Backend/routes/routers.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   createUserDetails,
   userLogin,
   verifyOtp,
   googleSignIn,
   googleSignUp,
-  // getProjectsList,
-  // getCoursesList,
-  // createCourse,
 } = require("../controllers/main");
 
 router.route("/signup").post(createUserDetails);
+
 router.route("/login").post(userLogin);
+
 router.route("/verifyOtp").post(verifyOtp);
+
 router.route("/auth/google-signin").post(googleSignIn);
+
 router.route("/auth/google-signup").post(googleSignUp);
-// router.route("/getProjects").get(getProjectsList);
-// router.route("/getCourses").post(getCoursesList);
-// router.route("/createCourse").post(createCourse);
+
 module.exports = router;
