d836bd712ea82c50230712f27194610211bdb395
YashKumar <marupillayeswanth123@gmail.com>
2025-08-23 20:18:32 +0530

    added emit to notification bar and also updated templates and css

--- Frontend/src/components/Notification.vue
+++ Frontend/src/components/Notification.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="main" v-if="visible">
-    <div id="text" v-for="({ data }, index) in data" :key="index">
+  <div class="notifications" v-if="isNotificationVisible">
+    <div class="text" v-for="({ data }, index) in data" :key="index">
       <router-link to="">
-        <span id="title" v-html="data.title"></span>
-        <span id="subtitle" v-html="data.subtitle"></span>
+        <span class="title" v-html="data.title"></span>
+
+        <span class="subtitle" v-html="data.subtitle"></span>
       </router-link>
-      <div id="timer" v-if="data.enable_timer && showTimeInHours">
+
+      <div class="timer" v-if="data.enable_timer && showTimeInHours">
         Ends in {{ timeLeft.hours }}h {{ timeLeft.minutes }}m
         {{ timeLeft.seconds }}s
       </div>
-      <div id="timer" v-else>{{ humanReadableTimeDiff }} left!</div>
+
+      <div class="timer" v-else>{{ humanReadableTimeDiff }} left!</div>
     </div>
-    <div id="icon" @click="closeNotification">
-      <CloseOutlined />
-    </div>
+
+    <CloseOutlined class="close-icon" @click="closeNotification" />
   </div>
 </template>
 
@@ -23,21 +25,28 @@ import axios from "axios";
 import { onUnmounted, ref, watch } from "vue";
 import { differenceInSeconds, formatDistanceToNow, parseISO } from "date-fns";
 
-const visible = ref(true);
+const isNotificationVisible = ref(true);
 const data = ref("");
 const timeLeft = ref({ hours: 0, minutes: 0, seconds: 0 });
 const endTime = ref("");
-const closeNotification = () => {
-  visible.value = false;
-};
 const showTimeInHours = ref(false);
 const humanReadableTimeDiff = ref();
+const emit = defineEmits(["closedNotification"]);
+
 localStorage.setItem("remainingTime", JSON.stringify(timeLeft));
+
 const fetchAPi = async () => {
   try {
     const response = await axios.get(
-      "https://www.udemy.com/api-2.0/notices/me/?type=smart_bar&limit=1"
+      "https://www.udemy.com/api-2.0/notices/me/",
+      {
+        params: {
+          type: "smart_bar",
+          limit: 1,
+        },
+      }
     );
+
     data.value = response.data.results;
     console.log(data.value);
     console.log(data.value[0].data.enable_timer);
@@ -109,6 +118,12 @@ const startCountdown = (endTime) => {
 // const saveRemainingTime = (timeLeft) => {
 //   localStorage.setItem("remainingTime", JSON.stringify(timeLeft));
 // };
+
+const closeNotification = () => {
+  isNotificationVisible.value = false;
+  emit("closedNotification", isNotificationVisible.value);
+};
+
 fetchAPi();
 
 watch(timeLeft, (newTimeLeft, oldTimeLeft) => {
@@ -127,17 +142,17 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
-.main {
+.notifications {
   display: flex;
   align-items: center;
   justify-content: center;
-  line-height: 1.2;
+  line-height: 1.4;
   background-color: #c2e9eb;
   color: #2f2d31;
   font-size: 16px;
 }
 
-#text {
+.text {
   padding: 16px 0px;
 }
 
@@ -147,33 +162,33 @@ a {
   cursor: pointer;
 }
 
-#icon {
+.close-icon {
   position: absolute;
   right: 0px;
   margin: 8px;
   padding: 0px 10px;
 }
 
-#icon:hover {
+.close-icon:hover {
   cursor: pointer;
 }
 
-#title {
+.title {
   padding: 0px 5px 0px 0px;
   font-weight: 700;
 }
 
-#subtitle {
+.subtitle {
   text-decoration: none;
   text-underline-offset: 0.4rem;
 }
 
-:deep(#subtitle a) {
+:deep(.subtitle a) {
   color: #2f2d31;
   text-underline-offset: 0.4rem;
 }
 
-#timer {
+.timer {
   font-weight: 700;
   font-size: 18px;
   text-align: center;
--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
-  <Notification />
-  <NavigationBar :isDashboardPage="false" />
+  <Notification @closedNotification="NotificationLabel" />
+  <NavigationBar
+    :isDashboardPage="false"
+    :isNotificationVisible="isNotificationVisible"
+  />
   <BannerSlide :isLogoutPage="false" />
   <Logos />
   <Learning />
@@ -10,7 +13,6 @@
     <CoursesCards :isLogoutPage="false" />
   </Suspense>
   <FocusedGoals />
-  <!-- <New /> -->
   <CaseStudies />
   <FooterBar />
 </template>
@@ -23,9 +25,16 @@ import Reports from "@/components/Reports.vue";
 import Trending from "@/components/Trending.vue";
 import Learning from "@/components/Learning.vue";
 import CoursesCards from "@/components/CoursesCards.vue";
-import New from "@/components/New.vue";
 import Notification from "@/components/Notification.vue";
 import BannerSlide from "@/components/BannerSlide.vue";
 import FocusedGoals from "@/components/FocusedGoals.vue";
 import CaseStudies from "@/components/CaseStudies.vue";
+import { ref } from "vue";
+
+const isNotificationVisible = ref(true);
+
+const NotificationLabel = (value) => {
+  console.log("NotificationLabel closed", value);
+  isNotificationVisible.value = value;
+};
 </script>
