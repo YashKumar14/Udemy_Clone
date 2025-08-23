2718c0195ee8dbfa67a6f4c0c6102a95253cf865
YashKumar <marupillayeswanth123@gmail.com>
2025-08-23 23:42:02 +0530

    updated util functions with user roles

--- Backend/utils/util.js
+++ Backend/utils/util.js
@@ -5,6 +5,30 @@ const bcrypt = require("bcrypt");
 const CLIENT_ID = process.env.CLIENT_ID;
 const client = new OAuth2Client(CLIENT_ID);
 
+const generateCid = () => {
+  return `cid:${Date.now()}@udemy.com`;
+};
+
+const getRoleId = async (role) => {
+  const getRoleQuery = `
+    SELECT role_id
+    FROM user_roles
+    WHERE role_name = $1
+  `;
+
+  try {
+    const roleIDResult = await pool.query(getRoleQuery, [role]);
+
+    if (roleIDResult.rowCount > 0) {
+      return roleIDResult.rows[0].role_id;
+    } else {
+      throw new Error("user role ID not found");
+    }
+  } catch (error) {
+    console.error("Error while fetching user roleId:", error.message);
+  }
+};
+
 const isUserExist = async (email) => {
   return await pool.query(
     `SELECT * FROM user_details
@@ -14,16 +38,14 @@ const isUserExist = async (email) => {
   );
 };
 
-const generateCid = () => {
-  return `cid:${Date.now()}@udemy.com`;
-};
-
 const createUser = async (fullname, email, role, password = email) => {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+  const roleID = await getRoleId(role);
+  console.log("roleID", roleID);
   const insertData = `
-    INSERT INTO user_details(user_fullname, user_email, password, user_role)
+    INSERT INTO user_details(user_fullname, user_email, password, user_role_id)
     VALUES($1, $2, $3,$4)`;
 
   try {
@@ -31,10 +53,46 @@ const createUser = async (fullname, email, role, password = email) => {
       fullname,
       email,
       hashedPassword,
-      role,
+      roleID,
     ]));
   } catch (error) {
     console.error("Error creating user:", error);
+  }
+};
+
+const getUserRole = async (roleID) => {
+  const getRoleQuery = `
+    SELECT role_name
+    FROM user_roles
+    WHERE role_id = $1
+  `;
+
+  try {
+    const roleResult = await pool.query(getRoleQuery, [roleID]);
+
+    if (roleResult.rowCount > 0) {
+      return roleResult.rows[0].role_name;
+    } else {
+      throw new Error("User role name not found");
+    }
+  } catch (error) {
+    console.error("Error while fetching user role:", error.message);
+  }
+};
+
+const getUserId = async (email) => {
+  const userIdQuery = `SELECT user_id FROM user_details WHERE user_email=$1`;
+
+  try {
+    const userResult = await pool.query(userIdQuery, [email]);
+
+    if (userResult.rowCount > 0) {
+      return userResult.rows[0].user_id;
+    } else {
+      throw new Error("User not Found");
+    }
+  } catch (error) {
+    console.error("Error while fetching user id:", error.message);
   }
 };
 
@@ -56,4 +114,12 @@ const verifyAndCheckUser = async (token) => {
   return { email, name, userExist };
 };
 
-module.exports = { isUserExist, generateCid, createUser, verifyAndCheckUser };
+module.exports = {
+  generateCid,
+  getRoleId,
+  isUserExist,
+  createUser,
+  getUserRole,
+  getUserId,
+  verifyAndCheckUser,
+};
