d3a38bbe6a9ef76c27751db53be6c694f864355a
YashKumar <marupillayeswanth123@gmail.com>
2025-11-05 15:00:29 +0530

    fix(auth): add timeout to login api

--- Frontend/src/views/LoginPage.vue
+++ Frontend/src/views/LoginPage.vue
@@ -232,9 +232,15 @@ const login = async () => {
   try {
     spinning.value = true;
     validationError.value = "";
-    const response = await axios.post(`${apiUrl}/login`, {
-      email: formData.value.email,
-    });
+    const response = await axios.post(
+      `${apiUrl}/login`,
+      {
+        email: formData.value.email,
+      },
+      {
+        timeout: 180000,
+      }
+    );
 
     console.log("response:::", response.data);
 
